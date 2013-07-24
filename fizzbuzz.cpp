44471395334f1ac0e2e22cfb6f3b14bf45225607
ssc-sbando <satoshi_bando@solver.co.jp>
2013-07-25 02:06:34 +0900

    applay updated source

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,37 +1,42 @@
-#include <sstream>
+#include <boost/lexical_cast.hpp>
 
 #include "fizzbuzz.h"
 
-static const std::string STR_FIZZ("fizz");
-static const std::string STR_BUZZ("buzz");
-
 /*
  * ・わかりやすさを優先
- * ・fizzbuzzの処理はfizzおよびbuzzと共通化
- * ・retrunは一ヶ所
+ * ・fizzbuzzの処理はfizzおよびbuzzの処理と共通化
+ * ・returnは一ヶ所
  */
 const std::string& fizzbuzz(int n){
 
-	static std::string str;
+	using std::string;
+	using boost::lexical_cast;
 
-	str.erase();
+	static string str;
+
+	//clear()の方が、空文字列の代入より早い
+	//clear()の方が、erase()より直感的にわかりやすい
+	str.clear();
 
 	if(n % 3 == 0)
 	{
-		str.append(STR_FIZZ);
+ 		//空文字列へのappend()の方が、代入より早い(気がする)
+		str.append("fizz");
 	}
 
 	if(n % 5 == 0)
 	{
-		str.append(STR_BUZZ);
+		str.append("buzz");
 	}
 
+ 	//empty()の方が、size() == 0より早い(気がする)
 	if(str.empty())
 	{
-		std::stringstream ss;
-
-		ss << n;
-		str = ss.str();
+		//ここが一番時間がかかる
+ 		//lexical_castの方が、stringstreamより早い
+ 		//lexical_castとsnprintfは速度的に大差なく
+		//lexical_castの方が、snprintfより直感的にわかりやすい
+		str = lexical_cast<string>(n);
 	}
 
 	return str;
